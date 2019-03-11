a45f22f4f4def6e9dd00246b38f5883eca4be200
abacus670 <abacus670@gmail.com>
2019-03-11 17:23:23 +0900

    a

--- gisload.js
+++ gisload.js
@@ -1,2 +1,2 @@
 <script type="text/javascript"
-        src="https://maps.googleapis.com/maps/api/js?key="></script>
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_x3dBn-BAoFke6xhFtcg7jAjOuGsCED8"></script>
